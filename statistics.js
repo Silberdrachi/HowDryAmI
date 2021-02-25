096002cd023a9b15c54ff3ab870c40cefde29ae5
mazunki <rolferen@gmail.com>
2021-02-25 22:03:06 +0100

    added cdf function

--- statistics.js
+++ statistics.js
@@ -40,3 +40,13 @@ function cnorm(x, mean=0, sd=1) {
     }
     return (1/2)*(1+sign*erf);
 }
+
+function ncdf(x, mean, std) {
+  var x = (x - mean) / std;
+  var t = 1 / (1 + .2315419 * Math.abs(x));
+  var d =.3989423 * Math.exp( -x * x / 2);
+  var prob = d * t * (.3193815 + t * ( -.3565638 + t * (1.781478 + t * (-1.821256 + t * 1.330274))));
+  if( x > 0 ) prob = 1 - prob;
+  return prob;
+}
+
