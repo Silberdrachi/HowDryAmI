fba093d50c7b928a11365db8f4529a783c49c9c3
mazunki <rolferen@gmail.com>
2021-02-25 10:19:20 +0100

    looking better and formulas seem to make sense?

--- data.js
+++ data.js
@@ -5,42 +5,56 @@ function simple_calculate_dryness(chance, rolls) {
 }
 
 function fact(n) {
-	var out=1;
-	for (var i=2; i<=n; i++) {
-		out *= i;
+	var out=new Big(1);
+	for (var i=new Big(2); i<=n; i++) {
+		out = out.times(i);
 	}
 	return out;
 }
 
 function Comb(n, k) {
-	return fact(n) / (fact(k) * fact(n-k));
+	return fact(n).div(fact(k).times(fact(n-k)));
 }
 
 function calculate_dryness(chance, rolls, desire) {
 	// similar to simple_calculate_dryness, but allows adding a goal instead
 	// basic formula is C * S * F where C includes permutations, since order doesn't matter
 	// S is the chance of successes, and F the chance of failures
-	if (desire > rolls) return 0;
-	if (rolls == 0) return 0;
-	
-	var C = Comb(rolls, desire); // permutes the following
-	var S = Math.pow(chance, desire); // assuming all successes in order
-	var F = Math.pow(1-chance, rolls-desire); // assuems all fails in order
+	if (desire > rolls) return Big(0);
+	if (rolls == 0) return Big(0);
 
-	return C*S*F;
+	var C = Big(Comb(rolls, desire)); // permutes the following
+	var S = Big(chance).pow(desire); // assuming all successes in order
+	var F = Big(1-chance).pow(rolls-desire); // assuems all fails in order
+
+	var dryness = C.times(S).times(F);
+	return dryness;
+}
+
+function str_to_num(s) {
+	if (s.search("/") === -1) {
+		return Number(s, 10);
+	} else {
+		return Number((s.split("/")[0] / s.split("/")[1]), 10);
+	}
+}
+
+function format_number(bignum) {
+	if (Big(0).eq(bignum)) {
+		return "Impossible";
+	} else {
+		return bignum.times(1000).toPrecision(9) + " \u2030"; // permille symbol: U+2030
+	}
 }
 
 function update_dryness() {
-	var chance = document.getElementById("chance").value;
-	var rolls = document.getElementById("rolls").value;
-	var desire = document.getElementById("desire").value;
+	var chance = str_to_num(document.getElementById("chance").value);
+	var rolls = str_to_num(document.getElementById("rolls").value);
+	var desire = str_to_num(document.getElementById("desire").value);
 
 	var drynessField = document.getElementById("drynessField");
 	
-	var dryness = calculate_dryness(chance, rolls, desire);
-	drynessField.innerHTML = dryness;
+	drynessField.innerHTML = format_number(calculate_dryness(chance, rolls, desire));
 
-	update_graph(dryness);
-
-	return;
+	return calculate_dryness(chance, rolls, desire);
 }
